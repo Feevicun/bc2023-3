0bc857d30c5bb0ddcc905185294be77f928e97ea
Feevicun <121824657+Feevicun@users.noreply.github.com>
2023-10-06 01:25:44 +0300

    initial commit

--- main.js
+++ main.js
@@ -1,59 +1,27 @@
-const fs = require('fs');
+const fs = require("fs").promises;
 
-// Читання JSON з файлу
-fs.readFile('data.json', 'utf8', (err, data) => {
-  if (err) {
-    console.error('Помилка читання файлу:', err);
-    return;
-  }
-
-  try {
-    // Розбір JSON
-    const jsonData = JSON.parse(data);
-
-    // Знаходження активу з найменшим значенням
-    const minAsset = findMinAsset(jsonData);
-
-    // Запис у файл output.txt
-    const outputText = `Інші резервні активи:${minAsset.value}`;
-    fs.writeFile('output.txt', outputText, 'utf8', (err) => {
-      if (err) {
-        console.error('Помилка запису у файл:', err);
-      } else {
-        console.log('Результати аналізу збережено у файл output.txt');
-      }
-    });
-  } catch (jsonError) {
-    console.error('Помилка парсингу JSON:', jsonError);
-  }
-});
-
-// Функція для знаходження активу з найменшим значенням
-function findMinAsset(data) {
-  let minAsset = { value: Infinity };
-
-  if (Array.isArray(data)) {
-    for (const item of data) {
-      const currentMinAsset = findMinAsset(item);
-      if (currentMinAsset.value < minAsset.value) {
-        minAsset = currentMinAsset;
-      }
-    }
-  } else if (typeof data === 'object') {
-    for (const key in data) {
-      if (data.hasOwnProperty(key)) {
-        const currentMinAsset = findMinAsset(data[key]);
-        if (currentMinAsset.value < minAsset.value) {
-          minAsset = currentMinAsset;
-        }
-      }
-    }
-  } else if (typeof data === 'number') {
-    // Якщо це число, порівнюємо його з поточним мінімумом
-    if (data < minAsset.value) {
-      minAsset.value = data;
+async function minvalue(jsonData) {
+  let min = 10000;
+  let text = "";
+  for (let list of jsonData) {
+    if (list.value < min) {
+      min = list.value;
+      text = list.txt;
     }
   }
-
-  return minAsset;
+  return `${text}:${min}`;
 }
+
+async function main() {
+  try {
+    const data = await fs.readFile("data.json", "utf-8");
+    const jsonData = JSON.parse(data);
+    const str = await minvalue(jsonData);
+    await fs.writeFile("output.txt", str);
+    console.log("Output.txt created successfully.");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+main();
