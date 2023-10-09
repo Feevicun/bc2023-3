8c7b374160b62c18c128d237786105307db77749
Feevicun <121824657+Feevicun@users.noreply.github.com>
2023-10-09 18:06:48 +0300

    main.js

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const fs = require("fs").promises;
 async function minvalue(jsonData) {
   let min = 10000;
   let text = "";
-  for (let list of jsonData) {
-    if (list.value < min) {
-      min = list.value;
-      text = list.txt;
+  for (let asset of jsonData) {
+    if (asset.value < min) {
+      min = asset.value;
+      text = asset.txt;
     }
   }
   return `${text}:${min}`;
@@ -17,8 +17,8 @@ async function main() {
     const data = await fs.readFile("data.json", "utf-8");
     const jsonData = JSON.parse(data);
     const str = await minvalue(jsonData);
-    await fs.writeFile("output.txt", str);
-    console.log("Output.txt created successfully.");
+    await fs.writeFile("output.txt", str, "utf-8");
+    console.log("Output.txt створено успішно.");
   } catch (err) {
     console.error(err);
   }
